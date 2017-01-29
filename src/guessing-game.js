53673c1e8a47778617993b47a45632914ff251cd
alena <alenamakey08@gmail.com>
2017-01-30 00:47:36 +0300

    implement binary search

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
-
+    constructor() {
+      this.max = 0;
+      this.min = 0;
+    }
+    devideInterval() {
+      return Math.round((this.max+this.min)/2);
+    }
     setRange(min, max) {
-
+      this.min = min;
+      this.max = max;
     }
 
     guess() {
-
+        return this.devideInterval();
     }
 
     lower() {
-
+      this.max = this.devideInterval();
     }
 
     greater() {
-
+      this.min = this.devideInterval();
     }
 }
 
